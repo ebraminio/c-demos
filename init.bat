070bbf2769815a4c9219c81d6b94fe84975e460f
Ebrahim Byagowi <ebrahim@gnu.org>
2015-05-02 01:58:12 +0430

    Replace git submodule with manual clones

--- init.bat
+++ init.bat
@@ -1,4 +1,9 @@
 @echo off
+if not exist freetype2 git clone --depth=1 http://git.sv.nongnu.org/r/freetype/freetype2.git
+if not exist glfw git clone --depth=1 https://github.com/glfw/glfw
+if not exist glew git clone --depth=1 https://github.com/omniavinco/glew-cmake glew
+if not exist harfbuzz git clone --depth=1 https://github.com/behdad/harfbuzz
+
 rmdir /s /q build 2>NUL
 mkdir build
 cd build
--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+[ -d freetype2 ] || git clone --depth=1 http://git.sv.nongnu.org/r/freetype/freetype2.git
+[ -d glfw ] || git clone --depth=1 https://github.com/glfw/glfw
+[ -d glew ] || git clone --depth=1 https://github.com/omniavinco/glew-cmake glew
+[ -d harfbuzz ] || git clone --depth=1 https://github.com/behdad/harfbuzz
+
 rm -rf build
 mkdir build
 cd build
