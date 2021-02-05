46b5bbaac23517bea1014781c24c0c898f92c1ed
Ebrahim Byagowi <ebrahim@gnu.org>
2021-02-05 22:49:30 +0330

    Add libwebp-decode example
    test.webp is from libwebp project itself

---
+++ libwebp-decode.c
@@ -0,0 +1,29 @@
+// gcc libwebp-decode.c `pkg-config --libs --cflags libwebp` -Wall && ./a.out
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <webp/decode.h>
+
+int main() {
+  uint8_t *buf;
+  unsigned len;
+  {
+    FILE *f = fopen("input/test.webp", "rb");
+    fseek(f, 0, SEEK_END);
+    len = ftell(f);
+    rewind(f);
+    buf = (uint8_t *)malloc(len);
+    len = fread(buf, 1, len, f);
+    fclose(f);
+  }
+  int width, height;
+  uint8_t *image = WebPDecodeRGB(buf, len, &width, &height);
+  {
+    FILE *f = fopen("out.pbm", "wb");
+    fprintf(f, "P6 %d %d 255\n", width, height);
+    fwrite(image, sizeof(char), width * height * 3, f);
+    fclose(f);
+  }
+  WebPFree(image);
+  return 0;
+}
